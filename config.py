7cff9c6526b67214b1f99112b1fff7ec2aeea71f
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-06-14 00:15:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Lists of first and last names for masking
+first_names = ["Emma", "Liam", "Olivia", "Noah", "Ava", "Sophia", "Isabella", "Mia", "Charlotte", "Amelia",
+               "Harper", "Evelyn", "Abigail", "Emily", "Elizabeth", "Mila", "Ella", "Avery", "Sofia", "Camila",
+               "Jackson", "Aiden", "Lucas", "Liam", "Noah", "Ethan", "Caden", "Logan", "Mason", "Oliver",
+               "Elijah", "Grayson", "Jacob", "Michael", "Benjamin", "Carter", "Alexander", "James", "Jayden",
+               "John", "Matthew", "David", "Joseph", "Daniel", "Henry", "Owen", "Wyatt", "Dylan", "Gabriel",
+               "William", "Nathan", "Samuel", "Andrew", "Jack", "Anthony", "Christopher", "Joshua", "Jaxon",
+               "Emily", "Chloe", "Grace", "Zoe", "Nora", "Hannah", "Lily", "Addison", "Aubrey", "Zoey",
+               "Elizabeth", "Ella", "Mila", "Scarlett", "Victoria", "Lillian", "Camilla", "Layla", "Penelope",
+               "Riley", "Aria", "Eleanor", "Hazel", "Aurora", "Lucy", "Audrey", "Bella", "Savannah", "Claire"]
+
+last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
+              "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson",
+              "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis",
+              "Robinson", "Walker", "Young", "Allen", "King", "Wright", "Scott", "Torres", "Nguyen", "Hill",
+              "Flores", "Green", "Adams", "Nelson", "Baker", "Hall", "Rivera", "Mitchell", "Carter", "Roberts",
+              "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sanchez",
+              "Morris", "Rogers", "Reed", "Cook", "Morgan", "Bell", "Murphy", "Bailey", "Cooper", "Richardson"]
