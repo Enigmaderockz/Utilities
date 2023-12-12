e14e12aa29e71a4204ecd4b0b298cb792f224d44
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-12-13 01:24:43 +0530

    Create exl2csv.py

---
+++ exl2csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def excel_to_pipe_delimited_csv(excel_file_path, csv_file_path):
+    # Read Excel file into a pandas DataFrame
+    df = pd.read_excel(excel_file_path)
+
+    # Save DataFrame to pipe-delimited CSV
+    df.to_csv(csv_file_path, sep='|', index=False)
+
+# Example usage
+excel_file_path = 'orig.xlsx'
+csv_file_path = 'orig.csv'
+
+excel_to_pipe_delimited_csv(excel_file_path, csv_file_path)
